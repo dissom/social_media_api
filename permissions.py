3a3c37d723d3fd89c7d76f8436f86ae7d0ea90ff
Andrii Zubchenko <dissom1987@gmail.com>
2024-07-01 22:23:50 +0300

    Added new custom permission class IsOwnerOrFollower

--- permissions.py
+++ permissions.py
@@ -7,3 +7,23 @@ class IsOwnerOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return obj.owner == request.user
+
+
+class IsOwnerOrFollower(BasePermission):
+    """
+    Custom permission to only allow owners
+    of an object or their followers to view it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return (
+                obj.owner == request.user
+                or request.user.id in obj.followers.values_list(
+                    "owner", flat=True
+                )
+                or request.user.id in obj.following.values_list(
+                    "owner", flat=True
+                )
+            )
+        return obj.owner == request.user
