f105a67e3afe0aac243760075c5a6c4cdedd4027
Andrii Zubchenko <dissom1987@gmail.com>
2024-07-01 22:35:34 +0300

    Implemented documentation for API endpoints

--- social/views.py
+++ social/views.py
@@ -5,49 +5,89 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 
-from permissions import IsOwnerOrReadOnly
+from permissions import IsOwnerOrFollower, IsOwnerOrReadOnly
 from social.models import Post, UserProfile
 from social.serializers import (
     FollowUnfollowSerializer,
     PostListSerializer,
     PostSerializer,
     UserProfileDetailSerializer,
-    UserProfileListSerializerView,
+    UserProfileListSerializer,
     UserProfileSerializer,
 )
 
 
 class UserProfileView(viewsets.ModelViewSet):
 
-    queryset = UserProfile.objects.all()
+    queryset = (
+        UserProfile.objects.all()
+        .prefetch_related("followers", "following")
+    )
     serializer_class = UserProfileSerializer
     permission_classes = (
-        IsOwnerOrReadOnly,
+        IsOwnerOrFollower,
         IsAuthenticated,
     )
 
     def get_queryset(self):
-        """Retrieve profiles with filters by owner"""
-        queryset = self.queryset.filter(
-            Q(owner=self.request.user.id)
-            | Q(followers=self.request.user.id)
-            | Q(following=self.request.user.id)
-        ).distinct()
+        """
+        Retrieve profiles with filters by owner,
+        birth_date and location
+        """
+        queryset = self.queryset
 
         owner = self.request.query_params.get("owner")
+        birth_date = self.request.query_params.get("birth_date")
+        location = self.request.query_params.get("location")
         if owner:
             owner = owner.split(",")
             print(owner)
             queryset = queryset.filter(owner__username__in=owner)
+
+        if birth_date:
+            queryset = queryset.filter(birth_date=birth_date)
+
+        if location:
+            queryset = queryset.filter(location__icontains=location)
+
         return queryset
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="owner",
+                type=OpenApiTypes.STR,
+                description="Filter by owner (ex. ?owner=owner_username)"
+            ),
+            OpenApiParameter(
+                name="birth_date",
+                type=OpenApiTypes.DATE,
+                description="Filter by birth_date "
+                            "(ex. ?birth_date=2000-06-25)"
+            ),
+            OpenApiParameter(
+                name="location",
+                type=OpenApiTypes.STR,
+                description="Filter by location (ex. ?location=owner_location)"
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """Filtering by owner, birth_date, location"""
+        return super().list(request, *args, **kwargs)
 
     def get_serializer_class(self):
         serializer = self.serializer_class
         if self.action == "retrieve":
             serializer = UserProfileDetailSerializer
         if self.action == "list":
-            serializer = UserProfileListSerializerView
+            serializer = UserProfileListSerializer
         return serializer
 
     def perform_create(self, serializer):
@@ -74,6 +114,7 @@ class FollowUnfollowView(APIView):
         return get_object_or_404(UserProfile, id=pk)
 
     def post(self, request) -> Response:
+        """Follow or unfollow profiles"""
         user_id = request.data.get("user_id")
         action = request.data.get("action")
 
@@ -95,7 +136,7 @@ class FollowUnfollowView(APIView):
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().select_related("owner")
     serializer_class = PostSerializer
     permission_classes = (
         IsOwnerOrReadOnly,
@@ -117,10 +158,19 @@ class PostViewSet(viewsets.ModelViewSet):
         )
 
         hashtag_params = self.request.query_params.get("hashtag")
+        created_at = self.request.query_params.get("created_at")
+        updated_at = self.request.query_params.get("updated_at")
 
         if hashtag_params:
             hashtags_params = hashtag_params.split(",")
             queryset = queryset.filter(hashtags__in=hashtags_params)
+
+        if created_at:
+            print(created_at)
+            queryset = queryset.filter(created_at__date=created_at)
+
+        if updated_at:
+            queryset = queryset.filter(updated_at__date=updated_at)
 
         return queryset.distinct()
 
@@ -132,3 +182,28 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="hashtag",
+                type=OpenApiTypes.STR,
+                description="Filter by hashtag (ex. ?hashtag=example)",
+            ),
+            OpenApiParameter(
+                name="created_at",
+                type=OpenApiTypes.DATE,
+                description="Filter by post create date "
+                            "(ex. ?created_at=2024-06-24)",
+            ),
+            OpenApiParameter(
+                name="updated_at",
+                type=OpenApiTypes.DATE,
+                description="Filter by post updated date "
+                            "(ex. ?updated_at=2024-06-24)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        """Filtering by hashtag"""
+        return super().list(request, *args, **kwargs)
