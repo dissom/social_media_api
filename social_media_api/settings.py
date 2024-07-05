2c19a43fe4cc2b26966656e0f5d3b593024def0d
Andrii Zubchenko <dissom1987@gmail.com>
2024-07-05 21:29:09 +0300

    Added pagination.

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -173,6 +173,9 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_PAGINATION_CLASS': "rest_framework.pagination."
+                                "LimitOffsetPagination",
+    'PAGE_SIZE': 2
 }
 
 
